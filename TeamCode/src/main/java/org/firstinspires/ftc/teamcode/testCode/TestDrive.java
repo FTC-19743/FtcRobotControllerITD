09a0b473c82f5bc358abaf742eafec3bcd89546b
ftc19743 <90291326+ftc19743@users.noreply.github.com>
2024-09-03 17:52:40 -0700

    Testing ACME Dashboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testCode/TestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testCode/TestDrive.java
@@ -7,39 +7,32 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.libs.TeamGamepad;
+import org.firstinspires.ftc.teamcode.libs.teamUtil;
 
 @Config
 @TeleOp(name = "Test Drive", group = "Test Code")
 public class TestDrive extends LinearOpMode {
+    public enum test {YES, NO, MAYBE};
+    public static test AAenumthing = test.YES;
     public static int botX = 72;
     public static int botY = 72;
+    public static double AMPLITUDE = 10;
+    public static double PHASE = 90;
+    public static double FREQUENCY = 0.5;
 
-    private static void logGamepad(Telemetry telemetry, Gamepad gamepad, String prefix) {
-        telemetry.addData(prefix + "Synthetic",
-                gamepad.getGamepadId() == Gamepad.ID_UNASSOCIATED);
-        for (Field field : gamepad.getClass().getFields()) {
-            if (Modifier.isStatic(field.getModifiers())) {
-                continue;
-            }
-
-            try {
-                telemetry.addData(prefix + field.getName(), field.get(gamepad));
-            } catch (IllegalAccessException e) {
-                // ignore for now
-            }
-        }
-    }
+    private TeamGamepad gp1 = new TeamGamepad();
+    private TeamGamepad gp2 = new TeamGamepad();
 
     @Override
     public void runOpMode() throws InterruptedException {
+        teamUtil.theOpMode = this;
         FtcDashboard dashboard = FtcDashboard.getInstance();
-        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
-
         //FtcDashboard.setDrawDefaultField(false); // enable to eliminate field drawing
-
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry()); // write telemetry to Driver Station and Dashboard
+        gp1.initilize(true); // Game Pads can be plugged into the computer
+        gp2.initilize(false);
 
         waitForStart();
 
@@ -48,12 +41,17 @@ public class TestDrive extends LinearOpMode {
         }
 
         while (opModeIsActive()) {
+
+            // Drawing stuff on the field
             TelemetryPacket packet = new TelemetryPacket();
             packet.fieldOverlay().drawImage("/images/BatBot.jpg", botX, botY, 18, 18);
             //packet.fieldOverlay().drawImage("/dash/powerplay.png", 0, 0, 144, 144);
-            logGamepad(telemetry, gamepad1, "gamepad1");
-            logGamepad(telemetry, gamepad2, "gamepad2");
             dashboard.sendTelemetryPacket(packet);
+
+            // Graphing stuff and putting stuff in telemetry
+            //telemetry.addData("Item", data)); // Anything written like this can be graphed against time.  Multiple items can be graphed together
+
+            telemetry.update();
             sleep(20);
         }
     }
