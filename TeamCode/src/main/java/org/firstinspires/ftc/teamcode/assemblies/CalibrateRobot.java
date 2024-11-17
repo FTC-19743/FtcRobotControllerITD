00ac79da71d36ab43edb56dc909ee7d62fe05b7d
ftc19743 <90291326+ftc19743@users.noreply.github.com>
2024-11-16 16:20:24 -0800

    calibrate robot opmode added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/assemblies/CalibrateRobot.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.assemblies;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.assemblies.Robot;
+import org.firstinspires.ftc.teamcode.libs.Blinkin;
+import org.firstinspires.ftc.teamcode.libs.OpenCVSampleDetector;
+import org.firstinspires.ftc.teamcode.libs.TeamGamepad;
+import org.firstinspires.ftc.teamcode.libs.teamUtil;
+
+@TeleOp(name = "CalibrateRobot", group = "LinearOpMode")
+public class CalibrateRobot extends LinearOpMode {
+
+    Robot robot;
+
+
+    public void runOpMode() {
+        teamUtil.init(this);
+
+
+        robot = new Robot();
+        robot.initialize();
+        robot.initCV(false);// TODO: false for competition
+
+
+        telemetry.addLine("Calibrating");
+        telemetry.update();
+
+        robot.calibrate();
+    }
+}
+
